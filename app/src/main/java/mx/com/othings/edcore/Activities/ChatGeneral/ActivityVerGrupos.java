be7d08357df7e22d8f396e796b0a973ca33f09eb
CynthiaEstrada <cynthiaestralos@live.com.mx>
2021-01-09 18:35:38 -0600

    Se ven PDF

---
+++ app/src/main/java/mx/com/othings/edcore/Activities/ChatGeneral/ActivityVerGrupos.java
@@ -0,0 +1,16 @@
+package mx.com.othings.edcore.Activities.ChatGeneral;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import mx.com.othings.edcore.R;
+
+public class ActivityVerGrupos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver_grupos);
+    }
+}
--- app/src/main/java/mx/com/othings/edcore/Activities/ChatGeneral/MenuChat.java
+++ app/src/main/java/mx/com/othings/edcore/Activities/ChatGeneral/MenuChat.java
@@ -7,18 +7,20 @@ import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import mx.com.othings.edcore.R;
 
 public class MenuChat extends AppCompatActivity {
 
-    private Button btnVerUsuaruios;
+    private CardView btnVerUsuaruios, cardGrupos;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acticity_menu_chat);
 
-        btnVerUsuaruios = findViewById(R.id.btnVerEstudiantes);
+        btnVerUsuaruios = findViewById(R.id.cardEstudiantesChat);
+        cardGrupos = findViewById(R.id.cardGruposChat);
 
         btnVerUsuaruios.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,5 +29,17 @@ public class MenuChat extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        cardGrupos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MenuChat.this, VerEstudiantes.class);
+                startActivity(intent);
+
+            }
+        });
+
+
     }
 }
